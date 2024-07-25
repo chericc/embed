71b23a192b0ae7a27c768fff8e3638bcb6a7e62a
chericc <1461021702@qq.com>
2024-07-25 00:46:26 +0000

    tmp upload, uart unfin

--- pico/hello/main.c
+++ pico/hello/main.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "xlog.h"
+#include "TinyFrame.h"
 
-void pin_configure()
+typedef enum {
+    MSG_TYPE_ECHO = 0x0,
+} MSG_TYPE;
+
+static TinyFrame s_tf_;
+static TinyFrame *s_tf = &s_tf_;
+
+void TF_WriteImpl(TinyFrame *tf, const uint8_t *buff, uint32_t len)
+{
+    uart_write_blocking(uart1, buff, len);
+}
+
+static void pin_configure()
 {
     const uint WIFI_BOARD_EN_PIN = 16;
 
@@ -11,12 +24,38 @@ void pin_configure()
     gpio_put(WIFI_BOARD_EN_PIN, 1);
 }
 
+static void uart_configure()
+{
+    uart_init(uart1, 115200);
+    gpio_set_function(6, GPIO_FUNC_UART);
+    gpio_set_function(7, GPIO_FUNC_UART);
+}
+
+static TF_Result my_tf_echo_listener(TinyFrame *tf, TF_Msg *msg)
+{
+    xlog_dbg("Got response: %s", msg->data);
+    return TF_CLOSE;
+}
+
 int main() 
 {
     stdio_init_all();
+    xlog_dbg("hello\n");
 
     pin_configure();
 
-    xlog_dbg("hello\n");
+    TF_InitStatic(s_tf, TF_MASTER);
+
+    while (1) {
+        uint8_t msg_data[] = "hello, world!";
+        TF_Msg msg;
+        TF_ClearMsg(&msg);
+        msg.type = MSG_TYPE_ECHO;
+        msg.data = msg_data;
+        msg.len = sizeof(msg_data);
+        TF_Query(s_tf, &msg, my_tf_echo_listener, 20);
+        sleep_ms(1000);
+    }
+
     return 0;
 }
